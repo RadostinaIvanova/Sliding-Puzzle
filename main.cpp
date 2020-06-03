65c5e53136f7e682d8b8f1f3bb7bda1da991b99c
Радостина Иванова <49326297+RadostinaIvanova@users.noreply.github.com>
2020-06-04 01:03:22 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,28 +3,28 @@
 
 int main() {
 	unsigned int start_puzzle1[PUZZLE_SIZE][PUZZLE_SIZE] = { { 2, 5, 1 },
-															 { 6, 3, 7 },
-															 { 4, 0, 8 } };
+								 { 6, 3, 7 },
+								 { 4, 0, 8 } };
 
 	unsigned int start_puzzle2[PUZZLE_SIZE][PUZZLE_SIZE] = { { 1, 0, 2 },
-															 { 4, 5, 3 },
-															 { 7, 8, 6 } };
+								 { 4, 5, 3 },
+								 { 7, 8, 6 } };
 
 	unsigned int start_puzzle3[PUZZLE_SIZE][PUZZLE_SIZE] = { { 8, 6, 7 },
-															 { 2, 5, 4 },
-		                                                     { 3, 0, 1 } };
+								 { 2, 5, 4 },
+		                                                 { 3, 0, 1 } };
 
 	unsigned int start_puzzle4[PUZZLE_SIZE][PUZZLE_SIZE] = { { 6, 8, 3 },
-															 { 4, 7, 2 },
-															 { 5, 1, 0 } };
+								 { 4, 7, 2 },
+								 { 5, 1, 0 } };
 
 	unsigned int start_puzzle5[PUZZLE_SIZE][PUZZLE_SIZE] = { { 8, 6, 7 },
-															 { 2, 5, 4 },
-															 { 3, 0, 1 } };
+								 { 2, 5, 4 },
+								 { 3, 0, 1 } };
 
 	unsigned int start_puzzle6[PUZZLE_SIZE][PUZZLE_SIZE] = { { 1,2,3 },
-															 { 0,4,6 },
-															 { 7,5,8 } };
+								 { 0,4,6 },
+								 { 7,5,8 } };
 
 
     Puzzle solvePuzz(start_puzzle2);
